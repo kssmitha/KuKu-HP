4aa1cc5842878f77f109747194c4bd5fc22c0ea1
kssmitha <mrksdofficial@gmail.com>
2025-06-03 15:32:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function connectToWA() {
   const prefix = config.PREFIX;
   //===========================
 
-  console.log("Connecting ❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️");
+  console.log("Connecting KuKu-HP");
   const { state, saveCreds } = await useMultiFileAuthState(
     __dirname + "/auth_info_baileys/"
   );
@@ -92,21 +92,21 @@ async function connectToWA() {
           require("./plugins/" + plugin);
         }
       });
-      console.log("❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️ installed successful ✅");
-      console.log("❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️ connected to whatsapp ✅");
+      console.log("KuKu-HP 🐝 installed successful ✅");
+      console.log("KuKu-HP 🐝 connected to whatsapp ✅");
 
-      let up = `❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️ connected successful ✅`;
-      let up1 = `Hello Robin, I made bot successful`;
+      let up = `KuKu-HP 🐝 connected successful ✅`;
+      let up1 = `Hello bee, I made bot successful`;
 
-      robin.sendMessage(ownerNumber + "@s.whatsapp.net", {
+      robin.sendMessage("94752425527@s.whatsapp.net", {
         image: {
-          url: `https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20cs.jpg`,
+          url: `https://i.ibb.co/W4HY6jpX/Chat-GPT-Image-Jun-3-2025-03-30-04-PM.png`,
         },
         caption: up,
       });
       robin.sendMessage("94705900209@s.whatsapp.net", {
         image: {
-          url: `https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20cs.jpg`,
+          url: `https://i.ibb.co/W4HY6jpX/Chat-GPT-Image-Jun-3-2025-03-30-04-PM.png`,
         },
         caption: up1,
       });
@@ -395,7 +395,7 @@ async function connectToWA() {
   });
 }
 app.get("/", (req, res) => {
-  res.send("hey, ❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️ started✅");
+  res.send("hey, KuKu HP started✅");
 });
 app.listen(port, () =>
   console.log(`Server listening on port http://localhost:${port}`)
