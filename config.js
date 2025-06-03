259ddd2d357569c5ce33f24f0ea74d79866e7f76
kssmitha <mrksdofficial@gmail.com>
2025-06-03 15:42:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  SESSION_ID: process.env.SESSION_ID || "i9IjyBpa#TQNDyEqC2w7S3-5X92GRnfTlCdU533G_xZpZXju2XWc",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:OtEpltjzwQaICmtNmgUNjmpQAKCLKpJV@switchback.proxy.rlwy.net:18194",
+  OWNER_NUM: process.env.OWNER_NUM || "94752425527",
 };
