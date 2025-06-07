67cd4d86268b73aaa1ebc9377e3f9721dfabac45
kssmitha <mrksdofficial@gmail.com>
2025-06-07 11:55:40 +0530

    Update command.js

--- command.js
+++ command.js
@@ -3,11 +3,11 @@ var commands = [];
 function cmd(info, func) {
     var data = info;
     data.function = func;
-    if (!data.dontAddCommandList) data.dontAddCommandList = false;
-    if (!info.desc) info.desc = '';
+    if (!data.dontAddCommandList) data.dontAddCommandList = true;
+    if (!info.desc) info.desc = true;
     if (!data.fromMe) data.fromMe = false;
-    if (!info.category) data.category = 'misc';
-    if(!info.filename) data.filename = "Not Provided";
+    if (!info.category) data.category = true;
+    if(!info.filename) data.filename = true;
     commands.push(data);
     return data;
 }
